a01d08f5ddb2e60e508ae808a5b3c426207453c4
SouthWan <2508598490@qq.com>
2024-12-21 20:31:10 +0800

    fix: null

--- database/cascades/ability_ability_api.php
+++ database/cascades/ability_ability_api.php
@@ -5,8 +5,8 @@ use Handyfit\Framework\Cascade\Cascade;
 use Handyfit\Framework\Cascade\Blueprint;
 
 return Cascade::configure()->withTable(
-    'ability_query_rule',
-    '能力查询规则'
+    'ability_ability_api',
+    'Table explain'
 )->withSchema(function (Schema $schema) {
 
     $schema->create(function (Blueprint $table) {
