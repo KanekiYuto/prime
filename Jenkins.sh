580a750d15af93f4746050aa25a0d6980936db30
SouthWan <2508598490@qq.com>
2024-12-23 20:51:43 +0800

    重命名

--- Jenkins.sh
+++ Jenkins.sh
@@ -13,13 +13,13 @@ function verify_docker_exists() {
     index=0
 
     for (( i = 0; i < ${#container[*]}; i++ )); do
-        echo "||# =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> [第 ${i} 项 - ${container[i]}] =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> #||"
+        echo "||# =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> [第 ${i} 项] =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> #||"
 
         # shellcheck disable=SC2046
         if [ $(docker ps -a -f name="${container[i]}") ]; then
-            echo "||# =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> [存在] =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> #||"
+            echo "||# =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> [${container[i]} - 存在] =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> #||"
         else
-            echo "||# =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> [不存在] =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> #||"
+            echo "||# =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> [${container[i]} - 不存在] =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> #||"
             index=$((index+1))
         fi
 
@@ -30,7 +30,9 @@ function verify_docker_exists() {
         echo "||# =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> [执行容器编排] =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> #||"
         docker-compose up -d
         echo "||# =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> [进入 (PHP) 容器执行 (composer)] =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> #||"
-        # docker exec -it -w /var/www/html "${container[1]}" pwd
+
+        docker exec -w /var/www/html "${container[1]}" composer config -g repos.packagist composer https://packagist.jp
+        docker exec -w /var/www/html "${container[1]}" composer config -l -g
 
         # 根据 vendor 目录判断是需要初始化还是更新包
         if [ -d "vendor" ];then
@@ -43,9 +45,18 @@ function verify_docker_exists() {
 
 echo "||# =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> [开始构建] =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> #||"
 
+# shellcheck disable=SC2154
+git pull origin "${Branch}"
+
 if [ ! -f ".env" ]; then
     echo "||# =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> [(.env)不存在 - 拷贝(.env.example) 到 (.env)] =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> #||"
     cp .env.example .env
+fi
+
+# 使用无 Nginx 方式配置
+if [ ! -f "docker-compose.yml" ]; then
+    echo "||# =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> [替换 (docker-compose.yam) 文件] =>=>=>=>=>=>=>=>=>=>=>=>=>=>=> #||"
+    mv docker-compose.no-nginx.yml docker-compose.yml
 fi
 
 verify_docker_exists
